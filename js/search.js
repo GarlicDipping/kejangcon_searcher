4bac47e6862afbfe67dd787a8fb0179c14f438db
Minsoo Kim <garlicdipping101@gmail.com>
2021-09-19 16:18:51 +0900

    검색결과 소팅로직 추가

--- js/search.js
+++ js/search.js
@@ -2,16 +2,16 @@
 var $htmlOrBody = $('html, body'); // scrollTop works on <body> for some browsers, <html> for others
 var scrollTopPadding = 8;
 
-function scrollSearchboxToTop(){
+function scrollSearchboxToTop() {
     var textareaTop = input.offsetTop;
     // scroll to the textarea
     var timing = 250;
-    $htmlOrBody.animate({ scrollTop: textareaTop - scrollTopPadding }, timing);    
+    $htmlOrBody.animate({ scrollTop: textareaTop - scrollTopPadding }, timing);
 }
 
 function setup() {
     input.addEventListener("focus", scrollSearchboxToTop);
-    input.addEventListener("keyup", function () {        
+    input.addEventListener("keyup", function () {
         var val = input.value.trim();
         search_result_table.innerHTML = '';
         if (!val) {
@@ -21,17 +21,47 @@ function setup() {
         }
         var regex = createFuzzyMatcher(val);
         var resultDatas = kejangcon_data.filter(function (row) {
-            return regex.test(row["tags"]);
+            for (const tag of row['tags']) {
+                if (regex.test(tag)) {
+                    return true;
+                }
+            }
+            return false;
+        }).map(function (row) {
+            let longestDistance = 0;
+            for (const tag of row['tags']) {
+                let dist = 0;
+                tag.replace(regex, (match, ...groups) => {
+                    const letters = groups.slice(0, val.length);
+                    let lastIndex = 0;
+                    for (let i = 0, l = letters.length; i < l; i++) {
+                        const idx = match.indexOf(letters[i], lastIndex);
+                        if (lastIndex > 0) {
+                            dist = Math.max(dist, idx - lastIndex);
+                        }
+                        lastIndex = idx + 1;
+                    }
+                });
+                if(dist > longestDistance){
+                    longestDistance = dist;
+                }
+            }
+            return { row, longestDistance };
         });
+        resultData.sort((a, b) => {
+            return a.longestDistance - b.longestDistance;
+          });
+
         search_result_text.innerHTML = "<p>" + resultDatas.length + "개 찻앗구...ㅎ</p>";
-        for(var i = 0; i < resultDatas.length; i++){
-            var imgName = resultDatas[i].pid;
-            var figElem = document.createElement('figure');            
+        for (var i = 0; i < resultDatas.length; i++) {
+            var row = resultDatas[i].row;
+            var imgName = row.pid;
+            var figElem = document.createElement('figure');
             var imgElem = document.createElement('img');
             imgElem.src = "./img/kejangcon/loading.gif";
-            tryImages(imgElem, resultDatas[i].group_id, imgName);
+            tryImages(imgElem, row.group_id, imgName);
             var figCaptionElem = document.createElement('figcaption');
-            figCaptionElem.textContent = '케장콘 ' + resultDatas[i].group_id;
+            figCaptionElem.textContent = '케장콘 ' + row.group_id;
             figElem.appendChild(imgElem);
             figElem.appendChild(figCaptionElem);
             search_result_table.appendChild(figElem);
@@ -43,11 +73,11 @@ function setup() {
 
 function tryImages(img, group_id, name) {
     img.src = "./img/kejangcon/" + group_id + "/" + name + ".png";
-    img.onerror = function() {
+    img.onerror = function () {
         img.src = "./img/kejangcon/" + group_id + "/" + name + ".jpg";
-        img.onerror = function() {
+        img.onerror = function () {
             img.src = "./img/kejangcon/" + group_id + "/" + name + ".gif";
-            img.onerror = function(){
+            img.onerror = function () {
                 img.src = "./img/kejangcon/error.png";
             }
         };
